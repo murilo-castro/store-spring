d34a2321ab2621e5909f363e153730c161f84492
Murilo Castro <murilo.65@gmail.com>
2025-05-29 00:12:22 -0300

    feat: Creating OrderStatus Enum

--- src/main/java/com/mtech/course/config/TestConfig.java
+++ src/main/java/com/mtech/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mtech.course.entities.Order;
 import com.mtech.course.entities.User;
+import com.mtech.course.entities.enums.OrderStatus;
 import com.mtech.course.repositories.OrderRepository;
 import com.mtech.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User alex = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), maria);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), alex);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), alex);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, maria);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITINING_PAYMENT, alex);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITINING_PAYMENT, alex);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
