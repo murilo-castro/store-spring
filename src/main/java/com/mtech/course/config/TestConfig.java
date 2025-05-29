abe1ff73e1d80fb0641aa1e2e038ebe7e696d54b
Murilo Castro <murilo.65@gmail.com>
2025-05-29 19:51:35 -0300

    feat: Created Product and Category association with JoinTable

--- src/main/java/com/mtech/course/config/TestConfig.java
+++ src/main/java/com/mtech/course/config/TestConfig.java
@@ -39,9 +39,13 @@ public class TestConfig implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User alex = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, maria);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITINING_PAYMENT, alex);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITINING_PAYMENT, alex);
+		userRepository.saveAll(Arrays.asList(maria, alex));
+		
+		Category electronics = new Category(null, "Electronics");
+		Category books = new Category(null, "Books");
+		Category computers = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(electronics, books, computers));
 		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
@@ -49,13 +53,23 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
-		Category electronics = new Category(null, "Electronics");
-		Category books = new Category(null, "Books");
-		Category computers = new Category(null, "Computers");
-		
-		userRepository.saveAll(Arrays.asList(maria, alex));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(electronics, books, computers));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(books);
+		p2.getCategories().add(computers);
+		p2.getCategories().add(electronics);
+		p3.getCategories().add(computers);
+		p4.getCategories().add(computers);
+		p5.getCategories().add(books);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, maria);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITINING_PAYMENT, alex);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITINING_PAYMENT, alex);
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		
 	}	
 }
