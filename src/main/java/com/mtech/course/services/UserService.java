f62b0f73ac6e3c9b9a39a6768a774383a32abe32
Murilo Castro <murilo.65@gmail.com>
2025-06-01 16:26:01 -0300

    feat: Creting exception handling findById

--- src/main/java/com/mtech/course/services/UserService.java
+++ src/main/java/com/mtech/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mtech.course.entities.User;
 import com.mtech.course.repositories.UserRepository;
+import com.mtech.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User user) {
@@ -32,6 +33,7 @@ public class UserService {
 	public User update(Long id, User user) {
 		User userEntity = userRepository.getReferenceById(id);
 		updateData(userEntity, user);
+		
 		return userRepository.save(userEntity);
 	}
 
