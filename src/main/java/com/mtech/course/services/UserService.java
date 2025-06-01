ea33b9d5280bc913c6036e710adc101f18f25fd8
Murilo Castro <murilo.65@gmail.com>
2025-06-01 15:18:46 -0300

    feat: deleteById in UserResource and Userservice

--- src/main/java/com/mtech/course/services/UserService.java
+++ src/main/java/com/mtech/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mtech.course.entities.User;
@@ -11,21 +12,25 @@ import com.mtech.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	public UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		
+
 		return user.get();
 	}
-	
+
 	public User insert(User user) {
 		return userRepository.save(user);
+	}
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
 	}
 }
